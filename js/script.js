50666ae8f97f820f6c0f586ba74bd3a39bb5aa36
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-03-02 17:06:46 +0100

    Completes first milestone

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// console.log('Vue Ready', Vue);
+
+// Vue.config.devtools = true;
+
+// const root = new Vue({
+//     el: '#root',
+//     data: {
+//         genres: [],
+//         selectedGenre: "",
+//         musicCards: [],
+//     },
+// });
